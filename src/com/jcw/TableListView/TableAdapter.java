c028f4cce656f05c655a54b5e08507633421ede3
Jackson Woodruff <woodruff.jackson@gmail.com>
2015-01-28 18:05:44 +0000

    filled in the TableAdapter to some extent

--- src/com/jcw/TableListView/TableAdapter.java
+++ src/com/jcw/TableListView/TableAdapter.java
@@ -25,12 +25,21 @@
 
 package com.jcw.TableListView;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Jackson on 1/28/2015.
  *
- * This is an adapter for use with the TableList
+ * This is an adapter for use with the TableList.
  *
  * It is especially designed to use String arrays
  * in such a ay that it can take the width of each
@@ -38,6 +47,84 @@ import android.widget.ArrayAdapter;
  * added to it (as opposed to someone having to
  * specify the size of each column).
  */
-public class TableAdapter extends ArrayAdapter<String[]> {
+public class TableAdapter extends ArrayAdapter<CharSequence[]> {
+	int[] columnWidths;
 
+	// These are some numbers used for storing various settings
+	// to do with the appearence of the list
+	int spaceColor = Color.TRANSPARENT;
+	int columnSpacing = 0;
+	int rowSpacing = 0;
+
+	int cellBackgroundColor = Color.TRANSPARENT;
+	
+	public TableAdapter(Context context, CharSequence[][] values) {
+		// 0 is okay to pass here because we are going to the the the 
+		super(context, 0, values);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		CharSequence[] contents = getItem(position);
+
+		if (columnWidths == null) {
+			throw new NullPointerException("You must call setColumnWidths before" +
+					" trying to draw the table");
+		}
+
+		// This layout is necessary in case we need to put the
+		// line in above the row.
+		LinearLayout tableRow = new LinearLayout(getContext());
+		tableRow.setOrientation(LinearLayout.VERTICAL);
+
+		if (position != 0) {
+			View rowDivider = new View(getContext());
+			LinearLayout.LayoutParams dividerParams = new LinearLayout.LayoutParams(
+					ViewGroup.LayoutParams.MATCH_PARENT, rowSpacing
+			);
+			rowDivider.setBackgroundColor(spaceColor);
+			tableRow.addView(rowDivider, dividerParams);
+		}
+
+		LinearLayout itemContainer = new LinearLayout(getContext());
+
+		for (int i = 0; i < Math.min(contents.length, columnWidths.length); i ++) {
+			if (i != 0) {
+				View columnDivider = new View(getContext());
+				LinearLayout.LayoutParams dividerParams = new LinearLayout.LayoutParams(
+						columnSpacing, ViewGroup.LayoutParams.MATCH_PARENT
+				);
+				columnDivider.setBackgroundColor(spaceColor);
+				itemContainer.addView(columnDivider, dividerParams);
+			}
+			TextView cell = new TextView(getContext());
+			LinearLayout.LayoutParams cellParams = new LinearLayout.LayoutParams(
+					columnWidths[i], ViewGroup.LayoutParams.MATCH_PARENT
+			);
+			cell.setBackgroundColor(cellBackgroundColor);
+			itemContainer.addView(cell, cellParams);
+		}
+
+		return tableRow;
+	}
+
+	public void setCellBackgroundColor(int color) {
+		this.cellBackgroundColor = color;
+	}
+
+	public void setSpaceColor(int color) {
+		this.spaceColor = color;
+	}
+
+	public void setColumnSpacing(int spacing) {
+		this.columnSpacing = spacing;
+	}
+
+	public void setRowSpacing(int spacing) {
+		this.rowSpacing = spacing;
+	}
+	
+	public void setColumnWidths(int[] widths) {
+		this.columnWidths = widths;
+	}
 }
